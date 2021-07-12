937a5c343b7c5a5ab00a405805ef674804d6f628
parix1999 <parisottoedoardo@gmail.com>
2021-07-12 16:21:13 +0200

    Vue

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,12 @@
 <template>
-    <div></div>
+    <div class="bg-black">
+        <div class="container">
+            <div>
+                -- Content Goes Here --
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -10,5 +17,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    .bg-black{
+        background-color: #161616;
+        color:white;
+        margin-top:20px;
+        height:80px;
+        line-height: 80px;  
+    }
+
 
 </style>
