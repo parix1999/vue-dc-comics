baa0685a7657e805c27542050b5628eaad46076f
parix1999 <parisottoedoardo@gmail.com>
2021-07-12 23:30:59 +0200

    Vue

--- src/components/Barra-iscrizione.vue
+++ src/components/Barra-iscrizione.vue
@@ -1,5 +1,22 @@
 <template>
-    <div></div>
+    <div class="bg-barra-sotto">
+        <div class="container spaziatura">
+            <div class="box-1">
+                SIGN-UP NOW!
+            </div>
+
+            <div>
+                <ul class="icon-social">
+                    <li>FOLLOW US</li>
+                    <li>
+                        <div class="box-social">
+                            <img src="../assets/footer-facebook.png" alt="social-logo">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,5 +27,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    
+    .bg-barra-sotto{
+        background-color:#2c2c2c;
+        padding:20px 0;
+    }
+
+    .spaziatura{
+        display:flex;
+        justify-content:space-between;
+        align-items: center;
+
+        .box-1{
+            color:white;
+            border: 2px solid #2465b1;
+            padding: 10px;
+        }
+    }
+    ul{
+        list-style-type: none;
+        li{
+            padding:0 10px;
+            &:first-child{
+                color:#2e76f9;
+            }
+        }
+    }
+    .icon-social{
+        display: flex;
+        align-items: center;;
+    }
+
 
 </style>
