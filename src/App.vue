dfc182d8365907cde1f3c32804a5b8343b6289d6
parix1999 <parisottoedoardo@gmail.com>
2021-07-12 15:19:27 +0200

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
+  <div id="app">
+    <NavBar />
+    <Content />
+    <BarraAzzurra />
+    <Footer />
+    <BarraIscrizione /> 
 
+  </div>
 </template>
 
 <script>
+import NavBar from './components/Nav-bar.vue';
+import Content from './components/Content.vue';
+import BarraAzzurra from './components/Barra-azzurra.vue';
+import Footer from './components/Footer.vue';
+import BarraIscrizione from './components/Barra-iscrizione.vue';
 
 
 export default {
   name: 'App',
   components: {
-    
+    NavBar,
+    Content,
+    BarraAzzurra,
+    Footer,
+    BarraIscrizione,
+
+
   }
 }
 </script>
