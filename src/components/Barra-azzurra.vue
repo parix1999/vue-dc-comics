8e52eea9ade4615914e5686a3c08c268d8b15c06
parix1999 <parisottoedoardo@gmail.com>
2021-07-12 15:42:25 +0200

    Vue

--- src/components/Barra-azzurra.vue
+++ src/components/Barra-azzurra.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>Ciao</div>
+    <div></div>
 </template>
 
 <script>
 export default {
-    name: 'Nav-bar',
+    name: 'Barra-Azzurra',
 }
 
 </script>
--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>Ciao</div>
+    <div class="container">
+        <div class="box-logo">
+            <img src="../assets/dc-logo.png" alt="logoDC">
+
+        </div>
+        
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Nav-bar',
 }
-
 </script>
 
+
 <style lang="scss" scoped>
+
+    .box-logo{
+        width:80px;
+        img{
+            width:100%;
+        }
+
+    }
 
 </style>
 
