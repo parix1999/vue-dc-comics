157712ba7cd1575311e175f750c293e64ff81190
parix1999 <parisottoedoardo@gmail.com>
2021-07-12 17:57:04 +0200

    Vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,49 @@
 <template>
     <div class="bg-pic">
-        <div class="container">
-            <div class="bg-logo">
+        <!-- <div class="container prova">
+            <div class="bg-logo"></div>
+        </div> -->
+        <div class="container separazione">
+            <ul>
+                <li>DC COMICS</li>
+                <li><a href="#">Charects</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+                <li class="shop">SHOP</li>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
 
-            </div>
+            </ul>
+            <ul>
+                <li>DC</li>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy(New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
 
+            </ul>
+            <ul>
+                <li>SITES</li>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Univverse</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+                
+            </ul>
+        
         </div>
+
     </div>
 </template>
 
@@ -32,6 +70,31 @@ export default {
             background-position:center right;
 
 
+        }
+        .separazione{
+            display: flex;
+            ul{
+                list-style: none;
+                padding:0 10px;
+
+                li a {
+                    text-decoration: none;
+                    color: gray;
+                    font-size:12px;
+
+                    
+                }
+                .shop{
+                    color:white;
+                    margin:10px 0;
+                }
+                li{
+                    &:first-child{
+                        color:white;
+                        margin:10px 0;
+                    }
+                }
+            }
         }
 
     }
