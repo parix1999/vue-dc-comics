26b03dc9390fbbbf5ee59d40bbbec4367cf55c7f
parix1999 <parisottoedoardo@gmail.com>
2021-07-12 17:06:00 +0200

    Vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-    <div></div>
+    <div class="bg-pic">
+        <div class="container">
+            <div class="bg-logo">
+
+            </div>
+
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,5 +17,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    .bg-pic{
+        height:400px;
+        background-image: url('../assets/footer-bg.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position:top;
+
+        .bg-logo{
+            height:400px;
+            background-image: url('../assets/dc-logo-bg.png');
+            background-repeat: no-repeat;
+            background-position:center right;
+
+
+        }
+
+    }
 
 </style>
